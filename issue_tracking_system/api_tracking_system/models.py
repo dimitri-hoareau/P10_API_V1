03b82ab60c317f9833f20e86dae9817d20b9eab8
dimitri-hoareau <dimitrihoareau2@gmail.com>
2021-12-24 13:58:06 +0100

    foreinkey issue project

--- issue_tracking_system/api_tracking_system/models.py
+++ issue_tracking_system/api_tracking_system/models.py
@@ -1,29 +1,30 @@
 from django.db import models
 from django.conf import settings
 
-class Contributors(models.Model):
-    # ROLE_CHOICES = (
-    #     ('1', 'Admin'),
-    #     ('2', 'User'),
-    # )
-
-    permission = models.CharField(max_length=255)
-    # role = models.CharField(max_length=1, choices=ROLE_CHOICES)
-    # user 
-    # project
-
-
 class Project(models.Model):
 
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     type = models.CharField(max_length=255)
-    # author
+
     # author = models.ForeignKey(
     #     to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+class Contributors(models.Model):
+    # ROLE_CHOICES = (
+    #     ('ADMIN', 'Admin'),
+    #     ('USER', 'User'),
+    # )
+
+    permission = models.CharField(max_length=255)
+    # role = models.CharField(max_length=5, choices=ROLE_CHOICES)
+
+
+    # project = models.ForeignKey(to=Project, on_delete=models.CASCADE)
+    # user 
 
 
 class Issue(models.Model):
@@ -34,12 +35,11 @@ class Issue(models.Model):
     priority = models.CharField(max_length=255)
     status = models.CharField(max_length=255)
     created_time = models.DateTimeField(auto_now_add=True)
-    # project
+
+
+    project = models.ForeignKey(to=Project, on_delete=models.CASCADE, null=True)
     # author
     # assignee
-
-
-    # category = models.ForeignKey('shop.Category', on_delete=models.CASCADE, related_name='products')
 
     def __str__(self):
         return self.title
@@ -49,11 +49,9 @@ class Comments(models.Model):
 
     description = models.TextField(blank=True)
     created_time = models.DateTimeField(auto_now_add=True)
-    # issue
+    
+    # issue = models.ForeignKey(to=Issue, on_delete=models.CASCADE)
     # author
-
-
-    # product = models.ForeignKey('shop.Product', on_delete=models.CASCADE, related_name='articles')
 
     def __str__(self):
         return self.description
